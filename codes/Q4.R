446ea37e55f6111f85cebdfedbafe622e0758ffd
Marcelo Tibau <marcelo@tibau.org>
2017-12-01 20:10:55 -0200

    Add files via upload

--- codes/Q4.R
+++ codes/Q4.R
@@ -3,33 +3,50 @@
 
 ## Tarefa 4: Gerar a analise de tamanho de efeito: comparativo de qualidade para cada instancia e pares de abordagens NSGAII x MAR , NSGAII x SH e NSGAII x CPM (pagina 1065)
 data_t4 <- read.table("data_t3-t4.txt", header = T)
-result <- list()
 DIGIT <- 4
 
-## Loop para gerar o subset com mean e SD 
-for (i in c('CPM', 'MAR', 'SH', 'nsga150k2x')) {
-  dataQfour <- data_t4[ which(data_t4$config == i), ]
-  result[[i]] <- cbind(
-    best = c(round(mean(dataQfour$best),  digits=DIGIT), round(sd(dataQfour$best),  digits=DIGIT)),
-    hv = c(round(mean(dataQfour$hv),  digits=DIGIT), round(sd(dataQfour$hv),  digits=DIGIT)),
-    gd = c(round(mean(dataQfour$gd),  digits=DIGIT), round(sd(dataQfour$gd),  digits=DIGIT))
-  )
+AMeasure <- function(r1, r2) 
+{
+  m <- length(r1);
+  n <- length(r2);
+  return ((sum(rank(c(r1, r2))[seq_along(r1)]) / m - (m + 1) / 2) / n);
 }
 
-# Construcao do Effect Size com base em https://www.leeds.ac.uk/educol/documents/00002182.htm
-effectsize <- function (one, two) {
-  effectsize_result <- cbind(
-    best = (one[1,][1] - two[1,][1]) / one[2,][1],
-    hv = (one[1,][2] - two[1,][2]) / one[2,][2],
-    gd = (one[1,][3] - two[1,][3]) / one[2,][3]
-  )
+data <- read.table("data_t3-t4.txt", header = TRUE)
+instances <- unique(data$inst)
+instance_names <- list(c("MAR", "SH", "CPM"), instances)
+best_result <- matrix(ncol = length(instances), nrow = 3, dimnames = instance_names)
+gd_result <- matrix(ncol = length(instances), nrow = 3, dimnames = instance_names)
+hv_result <- matrix(ncol = length(instances), nrow = 3, dimnames = instance_names)
+i <- 1
+
+for (instance in instances) {
+  mar_data = data[ which(data$config == 'MAR' & data$inst == instance), ]
+  sh_data = data[ which(data$config == 'SH' & data$inst == instance), ]
+  cpm_data = data[ which(data$config == 'CPM' & data$inst == instance), ]
+  nsga_data = data[ which(data$config == 'nsga150k2x' & data$inst == instance), ]
   
-  return(effectsize_result)
+  best_result[1, i] <- AMeasure(nsga_data$best, mar_data$best)
+  best_result[2, i] <- AMeasure(nsga_data$best, sh_data$best)
+  best_result[3, i] <- AMeasure(nsga_data$best, cpm_data$best)
+  
+  hv_result[1, i] <- AMeasure(nsga_data$hv, mar_data$hv)
+  hv_result[2, i] <- AMeasure(nsga_data$hv, sh_data$hv)
+  hv_result[3, i] <- AMeasure(nsga_data$hv, cpm_data$hv)
+  
+  gd_result[1, i] <- AMeasure(nsga_data$gd, mar_data$gd)
+  gd_result[2, i] <- AMeasure(nsga_data$gd, sh_data$gd)
+  gd_result[3, i] <- AMeasure(nsga_data$gd, cpm_data$gd)
+  
+  i <- i + 1
 }
 
-# Print das respostas
-print(effectsize(result$nsga150k2x, result$CPM))
-print(effectsize(result$nsga150k2x, result$MAR))
-print(effectsize(result$nsga150k2x, result$SH))
+print("Best ===============")
+print(best_result)
 
+print("HV ===============")
+print(hv_result)
+
+print("GD ===============")
+print(gd_result)
 
